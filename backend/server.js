ff9992a70a0375f7215cf38ff54a65aa85a795fe
Arshlankhann <arshlank894@gmail.com>
2025-08-10 20:25:49 +0530

    deploying on vercel

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 // server.js
 const allowedOrigins = [
     'http://localhost:5173',
-    'https://url-shortener-psi-plum.vercel.app'
+    'https://url-shortener-delta-weld.vercel.app/'
 ];
 
 app.use(cors({
